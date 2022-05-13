eff3bc1eb8af6739718570324ccd4e3dd5e80357
Arseniy <potyakinarseniy@gmail.com>
2022-05-14 00:24:04 +0300

    last commit

--- data/forms.py
+++ data/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField, SelectField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField, SelectField, IntegerField, FileField
+from wtforms.validators import DataRequired, ValidationError
+
+
+def lencheck(form, field):
+    if len(field.data) > 1080:
+        raise ValidationError('Поле должно быть меньше 1080 символов')
 
 
 class RegisterForm(FlaskForm):
@@ -8,6 +13,7 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
+    about = TextAreaField("Немного о себе")
     submit = SubmitField('Подтвердить')
 
 
@@ -20,13 +26,29 @@ class LoginForm(FlaskForm):
 
 class AddGoodForm(FlaskForm):
     name = StringField('Название товара', validators=[DataRequired()])
-    description = TextAreaField('Описание товара', validators=[DataRequired()])
+    description = TextAreaField('Описание товара', validators=[DataRequired(), lencheck])
     category = SelectField('Категория товара', choices=[('Еда'),
                                                         ('Бытовая техника'),
                                                         ('Электроника'),
                                                         ('Косметика'),
                                                         ('Одежда'),
-                                                        ('Мебель')])
+                                                        ('Мебель'),
+                                                        ('Другое')])
     price = IntegerField('Цена товара', validators=[DataRequired()])
-    pic_url = StringField('URL-ссылка на изображение')
     submit = SubmitField('Подтвердить')
+
+
+class SearchForm(FlaskForm):
+    title = StringField('')
+    category = SelectField('Категория', choices=[('☰ Каталог'),
+                                                 ('Еда'),
+                                                 ('Бытовая техника'),
+                                                 ('Электроника'),
+                                                 ('Косметика'),
+                                                 ('Одежда'),
+                                                 ('Мебель'),
+                                                 ('Другое')])
+    filtr = SelectField('Категория', choices=[('Сортировать по'),
+                                                 ('Цена ▲'),
+                                                 ('Цена ▼')])
+    submit = SubmitField('Поиск')
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-from flask import Flask, render_template, redirect, make_response, request, session, abort
+from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from markupsafe import escape
-from data.forms import RegisterForm, LoginForm, AddGoodForm
+from sqlalchemy import func
+from data.forms import RegisterForm, LoginForm, AddGoodForm, SearchForm
 import datetime
 import sqlite3
 from data import db_session
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
-app.config['SECRET_KEY'] = 'super-secret-key'
+UPLOAD_FOLDER = '/static/img/goods'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'KGqflOpYzMqfhmdEBFt1n6RmxKJm5uNa4WIcyj1trb9oIsiBvuUHfHwfZbXX'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+# модель базы данных товаров
 class Good(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
@@ -30,11 +34,14 @@ class Good(db.Model):
         return self.name
 
 
+# модель базы данных пользователей
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.Integer, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False, unique=True)
+    about = db.Column(db.Text)
+    cart = db.Column(db.String)
     created_date = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def __repr__(self):
@@ -47,18 +54,112 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password, password2)
 
 
-@app.route('/')
+# главная страница
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = SearchForm()
+    if form.validate_on_submit():
+        title = form.title.data.lower()
+        category = form.category.data
+        filtr = form.filtr.data
+        if category == '☰ Каталог':
+            category = None
+        if filtr == 'Сортировать по':
+            filtr = None
+        if filtr:
+            # сортировка в зависимости от выбранных значений
+            if filtr == 'Цена ▲':
+                if title and category:
+                    data = Good.query.filter(func.lower(Good.name).contains(title),
+                                             Good.category == category).order_by(Good.price).all()
+                elif title and not category:
+                    data = Good.query.filter(func.lower(Good.name).contains(title)).order_by(Good.price).all()
+                elif category and not title:
+                    data = Good.query.filter(Good.category == category).order_by(Good.price).all()
+                else:
+                    data = Good.query.order_by(Good.price).order_by(Good.price).all()
+            if filtr == 'Цена ▼':
+                if title and category:
+                    data = Good.query.filter(func.lower(Good.name).contains(title),
+                                             Good.category == category).order_by(Good.price.desc()).all()
+                elif title and not category:
+                    data = Good.query.filter(func.lower(Good.name).contains(title)).order_by(Good.price.desc()).all()
+                elif category and not title:
+                    data = Good.query.filter(Good.category == category).order_by(Good.price.desc()).all()
+                else:
+                    data = Good.query.order_by(Good.price.desc()).all()
+        else:
+            if title and category:
+                data = Good.query.filter(func.lower(Good.name).contains(title),
+                                         Good.category == category).all()
+            elif title and not category:
+                data = Good.query.filter(func.lower(Good.name).contains(title)).all()
+            elif category and not title:
+                data = Good.query.filter(Good.category == category).all()
+            else:
+                data = Good.query.order_by(Good.price).all()
+        return render_template('index.html', data=data, form=form)
     data = Good.query.order_by(Good.price).all()
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=data, form=form)
 
 
-@app.route('/purchase/<arc>/')
-def purchase(arc):
-    arc = int(arc)
+# корзина пользователя
+@app.route('/cart/<int:id>/')
+def cart(id):
+    db_sess = db_session.create_session()
+    data = db_sess.query(User).filter(User.id == current_user.get_id()).first()
+    sp = []
+    summa = 0
+    if data.cart:
+        splitter = map(int, data.cart.split())
+        for elem in splitter:
+            data2 = db_sess.query(Good).filter(Good.id == elem).first()
+            sp.append([f"{data2.id}", f"{data2.name}", f"{data2.price}"])
+            summa += int(data2.price)
+        return render_template('cart.html', sp=sp, summa=summa)
+    return render_template('cart.html', message='Ваша корзина пуста :(')
+
+
+# удаление товара из корзины пользователя
+@app.route('/delete_from_cart/<int:id>/')
+def delete_from_cart(id):
+    db_sess = db_session.create_session()
+    data = db_sess.query(User).filter(User.id == current_user.get_id()).first()
+    if data.cart:
+        new_cart = ''
+        cart = data.cart.split()
+        for elem in cart:
+            if int(elem) == id:
+                continue
+            else:
+                new_cart += str(elem)
+        new_cart = ' '.join(list(new_cart))
+        data.cart = new_cart
+        db_sess.commit()
+    return redirect(f'/cart/{current_user.get_id()}/')
+
+
+
+# удаление всез товаров из корзины
+@app.route('/clear_cart/<int:id>/')
+def clear_cart(id):
+    db_sess = db_session.create_session()
+    data = db_sess.query(User).filter(User.id == current_user.get_id(),
+                                      User.id == id).first()
+    if data.cart:
+        data.cart = None
+        db_sess.commit()
+    return redirect(f'/cart/{id}/')
+
+
+# подробная страница с товаром
+@app.route('/purchase/<int:id>/')
+def purchase(id):
+    # ниже код - полный колхоз, который был написан в первый день создания проекта и я не знаю
+    # что может быть уродливее этого. Но оно же работает, right? :D
     con = sqlite3.connect('database.db')
     cur = con.cursor()
-    res = cur.execute(f'''SELECT * FROM good WHERE id == {arc}''').fetchall()
+    res = cur.execute(f'''SELECT * FROM good WHERE id == {id}''').fetchall()
     name = res[0][1]
     description = res[0][2]
     category = res[0][3]
@@ -69,21 +170,37 @@ def purchase(arc):
     if current_user.is_authenticated:
         seller_id = int(result[0][0])
         buyer_id = int(current_user.get_id())
-        return render_template('purchase.html', arc=arc, name=name, description=description, category=category,
+        return render_template('purchase.html', arc=id, name=name, description=description, category=category,
                                price=price, pic_url=pic_url, user_nick=user_nick, buyer_id=buyer_id, seller_id=seller_id)
-    return render_template('purchase.html', arc=arc, name=name, description=description, category=category,
+    return render_template('purchase.html', arc=id, name=name, description=description, category=category,
                            price=price, pic_url=pic_url, user_nick=user_nick)
 
 
-@app.route('/delete/<arc>/')
-@login_required
-def delete(arc):
-    arc = int(arc)
+# добавление товара в корзину
+@app.route('/add_to_cart/<int:id>/')
+def add_to_cart(id):
     db_sess = db_session.create_session()
-    data = db_sess.query(Good).filter(Good.id == arc,
+    data = db_sess.query(User).filter(User.id == current_user.get_id()).first()
+    if data:
+        if data.cart:
+            data.cart = data.cart + ' ' + str(id)
+        else:
+            data.cart = str(id)
+        db_sess.commit()
+    return redirect(f'/purchase/{id}/')
+
+
+# удаление товара
+@app.route('/delete/<int:id>/')
+@login_required
+def delete(id):
+    db_sess = db_session.create_session()
+    data = db_sess.query(Good).filter(Good.id == id,
                                       Good.user_id == current_user.get_id()
                                       ).first()
     if data:
+        if data.pic_url != 'nophoto.jpg':
+            os.remove(f'static/img/goods/{data.pic_url}')
         db_sess.delete(data)
         db_sess.commit()
     else:
@@ -91,19 +208,21 @@ def delete(arc):
     return redirect('/')
 
 
-@app.route('/delete_account/<arc>/')
+# удаление аккаунта
+@app.route('/delete_account/<int:id>/')
 @login_required
-def delete_account(arc):
-    arc = int(arc)
+def delete_account(id):
     db_sess = db_session.create_session()
-    data = db_sess.query(User).filter(User.id == arc,
+    data = db_sess.query(User).filter(User.id == id,
                                       User.id == current_user.get_id()).first()
     if data:
         db_sess.delete(data)
         db_sess.commit()
-    data = db_sess.query(Good).filter(Good.user_id == arc).all()
+    data = db_sess.query(Good).filter(Good.user_id == id).all()
     if data:
         for elem in data:
+            if elem.pic_url != 'nophoto.jpg':
+                os.remove(f'static/img/goods/{elem.pic_url}')
             db_sess.delete(elem)
             db_sess.commit()
     else:
@@ -111,14 +230,14 @@ def delete_account(arc):
     return redirect('/')
 
 
-@app.route('/edit_account/<arc>/',  methods=['GET', 'POST'])
+# изменение данных аккаунта
+@app.route('/edit_account/<int:id>/', methods=['GET', 'POST'])
 @login_required
-def edit_account(arc):
-    arc = int(arc)
+def edit_account(id):
     form = RegisterForm()
     if request.method == "GET":
         db_sess = db_session.create_session()
-        info = db_sess.query(User).filter(User.id == arc,
+        info = db_sess.query(User).filter(User.id == id,
                                           User.id == current_user.get_id()).first()
         if info:
             form.email.data = info.email
@@ -129,29 +248,28 @@ def edit_account(arc):
             abort(404)
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        info = db_sess.query(User).filter(User.id == arc,
+        info = db_sess.query(User).filter(User.id == id,
                                           User.id == current_user.get_id()).first()
         if info:
             info.email = form.email.data
-            info.password = form.password.data
+            info.password = generate_password_hash(form.password.data)
             info.password_again = form.password_again.data
             info.name = form.name.data
             db_sess.commit()
-            return redirect(f'/profile/{arc}/')
+            return redirect(f'/profile/{id}/')
         else:
             abort(404)
     return render_template('register.html', form=form)
 
 
-
-@app.route('/edit/<arc>/',  methods=['GET', 'POST'])
+# изменение данных товара
+@app.route('/edit/<int:id>/',  methods=['GET', 'POST'])
 @login_required
-def edit(arc):
-    arc = int(arc)
+def edit(id):
     form = AddGoodForm()
     if request.method == "GET":
         db_sess = db_session.create_session()
-        info = db_sess.query(Good).filter(Good.id == arc,
+        info = db_sess.query(Good).filter(Good.id == id,
                                           Good.user_id == current_user.get_id()
                                           ).first()
         if info:
@@ -159,12 +277,11 @@ def edit(arc):
             form.description.data = info.description
             form.category.data = info.category
             form.price.data = info.price
-            form.pic_url.data = info.pic_url
         else:
             abort(404)
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        info = db_sess.query(Good).filter(Good.id == arc,
+        info = db_sess.query(Good).filter(Good.id == id,
                                           Good.user_id == current_user.get_id()
                                           ).first()
         if info:
@@ -172,20 +289,20 @@ def edit(arc):
             info.description = form.description.data
             info.category = form.category.data
             info.price = form.price.data
-            info.pic_url = form.pic_url.data
             db_sess.commit()
             return redirect('/')
         else:
             abort(404)
-    return render_template('add_good.html', form=form)
+    return render_template('add_good.html', form=form, msg='Изменить изображение нельзя.')
 
 
 
+# описание проекта Rulity
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
+# регистрация нового пользователя
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -206,6 +323,7 @@ def register():
         user = User(
             name=form.name.data,
             email=form.email.data,
+            about=form.about.data
         )
         user.set_password(form.password.data)
         db_sess.add(user)
@@ -214,6 +332,7 @@ def register():
     return render_template('register.html', form=form)
 
 
+# авторизация пользователя
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -230,60 +349,62 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+# добавление нового товара
 @app.route('/add_good', methods=['GET', 'POST'])
 @login_required
 def add_good():
     form = AddGoodForm()
     if form.validate_on_submit():
+        exceptions = ['jpg', 'png', 'raw', 'jpeg', 'tiff', 'gif', 'psd', 'bmp']
+        file = request.files['file']
+        filename = file.filename
+        if filename.split('.')[1].lower() not in exceptions:
+            filename = 'nophoto.jpg'
+        else:
+            try:
+                img = file.read()
+                with open(f'static/img/goods/{filename}', 'wb') as f:
+                    f.write(img)
+            except FileNotFoundError:
+                filename = 'nophoto.jpg'
+
         db_sess = db_session.create_session()
         good = Good(
             name=form.name.data,
             description=form.description.data,
             category=form.category.data,
             price=form.price.data,
-            pic_url=form.pic_url.data,
+            pic_url=filename,
             user_id=current_user.id
         )
+
         db_sess.add(good)
         db_sess.commit()
+
         return redirect('/')
     return render_template('add_good.html', form=form)
 
 
+# часто задаваемые вопросы касательно работы сайты
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
 
 
-@app.route('/add', methods=["GET", "POST"])
-def add():
-    if request.method == "POST":
-        name = request.form['name']
-        description = request.form['description']
-        category = request.form['category']
-        price = request.form['price']
-
-        good = Good(name=name, description=description, category=category, price=price)
-        try:
-            db.session.add(good)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'ошибка'
-    else:
-        return render_template('add.html')
-
+# страница для "пустых" кнопок))
 @app.route('/another')
 def another():
     return render_template('another.html')
 
 
+# загрузка текущего пользователя
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
 
+# выход из аккаунта
 @app.route('/logout')
 @login_required
 def logout():
@@ -291,7 +412,8 @@ def logout():
     return redirect("/")
 
 
-@app.route('/profile/<id>/')
+# профиль текущего пользователя
+@app.route('/profile/<int:id>/')
 def profile(id):
     db_sess = db_session.create_session()
     data = db_sess.query(User).filter(User.id == id).first()
@@ -309,4 +431,4 @@ def profile(id):
 
 if __name__ == "__main__":
     db_session.global_init("database.db")
-    app.run(debug=True)
+    app.run()
